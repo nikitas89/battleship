00ffde83b2d218b2ebfe46d170d09a16f49eb2bd
nikitas89 <cvnikita1@gmail.com>
2017-10-14 21:00:30 +0800

    debugged row 0 issues

--- script.js
+++ script.js
@@ -25,13 +25,14 @@ function placeShip(ship) {
 }
 
 function placeShipHorz(ship) {
+  console.log('placing horz now... '+ship);
   var v = Math.floor(Math.random() * (8))
   var emptyIndexes = []
-  console.log('emptyIndexes', emptyIndexes); //does this reset each time emtpyindex is called
-  for (var k = 0; k < 8; k++) {
+  // console.log('emptyIndexes', emptyIndexes); //does this reset each time emtpyindex is called
+  for (var k = 0; k < 8-ship; k++) {
     emptyIndexes.push(k)
   }
-  for (var z = 0; z < 8; z++) {
+  for (var z = 0; z < 8-ship; z++) { //this code is unique to hor/vert. combine the rest
     for (var x = 0; x < ship; x++) {
       if (grid[v][z + x] === 0)
         ""
@@ -43,44 +44,57 @@ function placeShipHorz(ship) {
     }
   }
   //get
+  console.log('emptyIndexes.length ',emptyIndexes.length);
   var shipPosIndex = emptyIndexes[Math.floor(Math.random() * emptyIndexes.length)]
   // console.log('shipPosIndex',shipPosIndex)
 
   //place the ship
   for (var i = shipPosIndex; i < shipPosIndex + ship; i++) {
+    console.log('placed '+ ship );
     grid[v][i] = ship
   }
-  console.log(grid)
+  // console.log(grid)
 } //end placeShipHorz
 function placeShipVert(ship) {
+    console.log('placing vert now... '+ship);
   var h = Math.floor(Math.random() * (8))
   var emptyIndexes = []
-  for (var k = 0; k < 8; k++) {
+  for (var k = 0; k < 8-ship; k++) {
     emptyIndexes.push(k)
   }
-  for (var z = 0; z < 8; z++) {
+
+
+  for (var z = 0; z < 8-ship; z++) {
     for (var x = 0; x < ship; x++) {
       if (x + z > 7) {
         emptyIndexes.splice(emptyIndexes.indexOf(z), 1)
+        // console.log('vert exceeds col ','h', h, 'z, ',z, 'x, ',x );
         break
       } else if (grid[z + x][h] === 0) {
         ""
       } else {
         emptyIndexes.splice(emptyIndexes.indexOf(z), 1)
-        break; //prevent multiple z splice.
+        // console.log('vert occupied ','h',h, 'z, ',z, 'x, ',x );
+        break; //prevent multiple z splice. break x for loop
       }
       // console.log('emptyIndexes', z, x, emptyIndexes)
       // console.log('grid value', grid[z + x][h])
     }
   }
+
+
+
+
   // console.log(emptyIndexes)
+  console.log('emptyIndexes.length ',emptyIndexes.length); //what to do if emptyindixes is 0? call random no and run for loop again.
   var shipPosIndex = emptyIndexes[Math.floor(Math.random() * emptyIndexes.length)]
   // console.log('shipPosIndex',shipPosIndex)
   //place the ship
   for (var i = shipPosIndex; i < shipPosIndex + ship; i++) {
+    console.log('placed '+ ship );
     grid[i][h] = ship
   }
-  console.log('final grid', grid)
+  // console.log('final grid', grid)
 } //end placeShipVert
 
 //horz placement - find start point randomly.
@@ -141,26 +155,30 @@ var winningShots = 0
 // var shot=22
 //change this
 function checkShot(shot) {
-  shot = shot - 11
+  //dont remove 1,1 yet.
+  // shot = shot
+  console.log('arrshot ',shot);
+  console.log(typeof(shot));
   var shotString = shot //check this code.
   shotString = shotString.toString()
-  var i = shotString.charAt(0)
-  var j = shotString.charAt(1)
+  console.log('shotString ',shotString);
+  var row = Number(shotString.charAt(0) -1)
+  var col = Number(shotString.charAt(1) -1)
   //check if the grid has this shot.
-  if (grid[i][j]) {
-    userGrid[i][j] = "Y"
+  if (grid[row][col]) {
+    userGrid[row][col] = "Y"
     winningShots++
-    setText(shot + 11, "Y") //check this code.
+    setText(Number(shot), "Y") //check this code.
   } else {
-    userGrid[i][j] = "X"
-    setText(shot + 11, "X")
+    userGrid[row][col] = "X"
+    setText(Number(shot), "X")
   }
   turns++
-  console.log(userGrid)
-  console.log(grid[i][j]);
+  console.log('userGrid ', userGrid)
+  console.log('row,',row, 'col', col);
 
 }
-
+//not working. check this.
 function checkWin() {
   if (turns < 51 && winningShots === 17)
     return true
@@ -175,16 +193,17 @@ $(function() {
   var $box = $('.box')
   // var $submit = $('#submit')
   $box.on('click', function() {
-    if (checkWin())
+    if (checkWin()) //
       alert("you won!")
     else {
-      //refactor to get input from grid : shot = $(this).attr('data-id')
+      //get input from grid : shot = $(this).attr('data-id')
       //var shot =  $('input').val()
       var shot = $(this).attr('data-id')
-      checkShot(shot)
+      checkShot(shot) //have added 1,1 to these.
+      if (checkWin()) //
+        alert("you won!")
     }
   })
-
 })
 //sets div with data-id of shot, to X or Y depending on if there is a ship.
 function setText(shot, text) {
